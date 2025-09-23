de5bbeb8da684442f47bb59fb5cc7d3340423c14
Sankalp-2664 <chakresankalp26@gmail.com>
2025-09-24 02:43:34 +0530

    Added Event related classes

---
+++ AgdataReward/Domain/Entities/EventDefinition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public sealed class EventDefinition
+{
+    public Guid Id { get; }
+    public string Code { get; }
+    public string Title { get; }
+
+    public EventDefinition(Guid id, string code, string title)
+    {
+        Id = id;
+        Code = code;
+        Title = title;
+    }
+}
+
---
+++ AgdataReward/Domain/Entities/EventInstance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class EventInstance
+{
+    public Guid Id { get; }
+    public Guid EventId { get; }
+    public Guid? WinnerUserId { get; private set; }
+    public int? Rank { get; private set; }
+
+    public EventInstance(Guid id, Guid eventId)
+    {
+        Id = id;
+        EventId = eventId;
+    }
+
+    public void AssignWinner(Guid userId, int rank)
+    {
+        WinnerUserId = userId;
+        Rank = rank;
+    }
+}
+

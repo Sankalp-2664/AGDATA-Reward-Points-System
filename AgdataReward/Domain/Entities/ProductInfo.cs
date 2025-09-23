068d008b1fa19f13eee447743078597790870def
Sankalp-2664 <chakresankalp26@gmail.com>
2025-09-24 01:06:10 +0530

    Created ProductInfo Class

---
+++ AgdataReward/Domain/Entities/ProductInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public sealed class ProductInfo
+{
+    public Guid Id { get; }
+    public string SKU { get; }
+    public string Name { get; }
+    public Guid RewardPointsId { get; }
+
+    public ProductInfo(Guid id, string sku, string name, Guid rewardPointsId)
+    {
+        Id = id;
+        SKU = sku;
+        Name = name;
+        RewardPointsId = rewardPointsId;
+    }
+}

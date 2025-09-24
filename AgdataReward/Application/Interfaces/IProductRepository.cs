565fd147d7763e1a1e066a7b6f413884edca8989
Sankalp-2664 <chakresankalp26@gmail.com>
2025-09-24 16:10:53 +0530

    Added Interfaces

---
+++ AgdataReward/Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces;
+
+public interface IProductRepository
+{
+    Task<ProductInfo?> GetByIdAsync(Guid id);
+    Task<ProductInfo?> GetBySkuAsync(string sku);
+    Task AddAsync(ProductInfo product);
+    Task<IEnumerable<ProductInfo>> ListAsync();
+}
---
+++ AgdataReward/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces;
+
+public interface IUserRepository
+{
+    Task<UserProfile?> GetByIdAsync(Guid id);
+    Task<UserProfile?> GetByEmailAsync(string email);
+    Task<UserProfile?> GetByEmployeeIdAsync(string employeeId);
+    Task AddAsync(UserProfile user);
+    Task<IEnumerable<UserProfile>> ListAsync();
+}

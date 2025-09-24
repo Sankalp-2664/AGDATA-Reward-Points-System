26503fa0db16c05decba58debb19fd18f75248fd
Sankalp-2664 <chakresankalp26@gmail.com>
2025-09-24 17:31:34 +0530

    Added services interfaces

---
+++ AgdataReward/Application/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ProductInfo> AddProductAsync(string sku, string name, Guid rewardPointsId);
+        Task<IEnumerable<ProductInfo>> GetCatalogAsync();
+    }
+}
---
+++ AgdataReward/Application/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserProfile> RegisterUserAsync(string employeeId, string email, string firstName, string lastName);
+        Task<UserProfile?> GetUserByEmailAsync(string email);
+        Task<UserAccount?> GetUserAccountAsync(Guid userId);
+    }
+
+}

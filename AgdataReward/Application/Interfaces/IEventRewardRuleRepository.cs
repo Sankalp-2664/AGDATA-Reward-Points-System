13faf58d6d1d60c7a7c839313104a659381ed02d
Sankalp-2664 <chakresankalp26@gmail.com>
2025-09-24 16:18:33 +0530

    Added Event related Interfaces

---
+++ AgdataReward/Application/Interfaces/IEventRewardRuleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IEventRewardRuleRepository
+    {
+        Task<IEnumerable<EventRewardRule>> GetByEventIdAsync(Guid eventId);
+        Task AddAsync(EventRewardRule rule);
+        Task UpdateAsync(EventRewardRule rule);
+    }
+}

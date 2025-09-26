ba43d1875082ff40c82fb73ffec0d321dd7eb086
Sankalp-2664 <chakresankalp26@gmail.com>
2025-09-26 13:31:36 +0530

    Added Exceptions

---
+++ AgdataReward/Domain/Exceptions/InsufficientPointsException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public sealed class InsufficientPointsException : DomainException
+    {
+        public InsufficientPointsException(int available, int required)
+            : base($"Insufficient points. Available: {available}, Required: {required}.") { }
+    }
+}
---
+++ AgdataReward/Domain/Exceptions/InvalidRedemptionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public sealed class InvalidRedemptionException : DomainException
+    {
+        public InvalidRedemptionException(string reason)
+            : base($"Invalid redemption: {reason}") { }
+    }
+}
